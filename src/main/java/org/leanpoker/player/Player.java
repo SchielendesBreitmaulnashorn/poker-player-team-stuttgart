cd2a880a958ff76512296ad2f85c478df8631e31
SchielendesBreitmaulnashorn <33595377+SchielendesBreitmaulnashorn@users.noreply.github.com>
2017-11-13 13:56:40 +0100

    Update Player.java
    next try

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 100;
+        return 100 + (int) (Math.random()*66.0d);
     }
 
     public static void showdown(JsonElement game) {

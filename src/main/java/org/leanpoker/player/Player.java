913e21d26c506aa0b01dd480b5772ed34a94b108
SchielendesBreitmaulnashorn <33595377+SchielendesBreitmaulnashorn@users.noreply.github.com>
2017-11-13 17:05:06 +0100

    Update Player.java
    next try

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "1.3";
+    static final String VERSION = "1.4";
 
     public static int betRequest(JsonElement request) {
     	JsonObject gameState = request.getAsJsonObject();
@@ -21,13 +21,40 @@ public class Player {
     	int ourStack = getOurStack(players);
     	int ourBet = getOurBet(players);
     	int ourMinimumRaise = currentByIn - ourBet + minimumRaise;
-    	if (distance < 3) {
-    		return ourMinimumRaise;
-    	} else if (distance < 5) {
-    		return ourMinimumRaise + 100;
-    	} else {
-    		return ourStack;
+    	int ourCall = currentByIn - ourBet;
+    	int round = gameState.get("round").getAsInt();
+    	
+    	if (ourStack > 4000) {
+    		return 0;
     	}
+    	if (distance == 0) {
+    		switch (round) {
+    		case 0: return gameState.get("small_blind").getAsInt();
+    		case 1:
+    		case 2:
+    		case 3:
+    			return ourMinimumRaise;
+    		}
+    	} else {
+    		switch (round) {
+    		case 0:
+    		case 1:
+    		case 2:
+    		case 3:
+    			return ourCall;
+    		}
+    	}
+    	if (round == 4 && ourStack == 1000) {
+    		return 1000;
+    	}
+    	return ourMinimumRaise;
+//    	if (distance < 3) {
+//    		return ourMinimumRaise;
+//    	} else if (distance < 5) {
+//    		return ourMinimumRaise + 100;
+//    	} else {
+//    		return ourStack;
+//    	}
 //    	System.out.println("state:" + "dealer=" + dealer + ", firstPlayer=" + firstPlayer + ", ourPlayer=" + ourPlayer + ", distance=" + distance);
 //    	switch (distance) {
 //    	case 0: 
@@ -86,3 +113,4 @@ public class Player {
     public static void showdown(JsonElement game) {
     }
 }
+

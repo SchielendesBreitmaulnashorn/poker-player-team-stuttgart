43d22a1f879938dd600d30a0e75ed87c8ddf529c
SchielendesBreitmaulnashorn <33595377+SchielendesBreitmaulnashorn@users.noreply.github.com>
2017-11-13 16:48:18 +0100

    Update Player.java
    try 1.3

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,7 +6,7 @@ import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "1.2";
+    static final String VERSION = "1.3";
 
     public static int betRequest(JsonElement request) {
     	JsonObject gameState = request.getAsJsonObject();
